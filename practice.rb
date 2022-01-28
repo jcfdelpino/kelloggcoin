7a005f6b576791f68571ac134907af37cb9ef9f1
jcfdelpino <juan.delpino@kellogg.northwestern.edu>
2022-01-28 04:01:03 +0000

    first jab

--- practice.rb
+++ practice.rb
@@ -27,3 +27,77 @@ blockchain = [
 # Anthony's KelloggCoin balance is 2650
 
 # 👇👇👇 Your code HERE 👇👇👇
+
+
+ben_total = 0
+brian_total = 0
+evan_total = 0
+anthony_total = 0
+
+## How can I loop through the different names?
+## ideas: use a loop to create a variable for each name. then use those variables and incorporate in the loop
+#ben withdrawals
+for row in blockchain
+    if row [:from_user] == 'ben'
+    amount= row [:amount]
+    ben_total = ben_total - amount
+ end
+end
+#ben income
+for row in blockchain
+  if row [:to_user] == 'ben'
+    amount= row [:amount]
+    ben_total = ben_total + amount
+ end
+end
+
+#brian withdrawals
+for row in blockchain
+    if row [:from_user] == 'brian'
+    amount= row [:amount]
+    brian_total = brian_total - amount
+ end
+end
+#brian income
+for row in blockchain
+  if row [:to_user] == 'brian'
+    amount= row [:amount]
+    brian_total = brian_total + amount
+ end
+end
+
+#evan withdrawals
+for row in blockchain
+    if row [:from_user] == 'evan'
+    amount= row [:amount]
+    evan_total = evan_total - amount
+ end
+end
+#evan income
+for row in blockchain
+  if row [:to_user] == 'evan'
+    amount= row [:amount]
+    evan_total = evan_total + amount
+ end
+end
+
+#anthony withdrawals
+for row in blockchain
+    if row [:from_user] == 'anthony'
+    amount= row [:amount]
+    anthony_total = anthony_total - amount
+ end
+end
+
+#anthony income
+for row in blockchain
+  if row [:to_user] == 'anthony'
+    amount= row [:amount]
+    anthony_total = anthony_total + amount
+ end
+end
+
+puts "Ben's KelloggCoin balance is #{ben_total}" 
+puts "Brian's KelloggCoin balance is #{brian_total}"
+puts "Evan's KelloggCoin balance is #{evan_total}"
+puts "Anthony's KelloggCoin balance is #{anthony_total}"
